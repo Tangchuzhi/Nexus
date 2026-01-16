a650ff32512593506ea3cc677bad2978b71c0d6e
唐初稚 <382944938@qq.com>
2026-01-16 23:31:45 +0800

    Update ui.sh

--- core/ui.sh
+++ core/ui.sh
@@ -122,7 +122,7 @@ show_disclaimer() {
     echo -e "  ${COLOR_GRAY}•${COLOR_RESET} 协议遵循: ${COLOR_CYAN}CC BY-NC-ND 4.0${COLOR_RESET}"
     echo -e "    ${COLOR_GRAY}(署名-非商业性使用-禁止演绎)${COLOR_RESET}"
     echo ""
-    echo -e "  ${COLOR_MAGENTA}👤 作者:${COLOR_RESET} 唐初稚 (Discord)"
-    echo -e "  ${COLOR_MAGENTA}🏝️ 发布:${COLOR_RESET} 游鹿小岛"
+    echo -e "  ${COLOR_MAGENTA}作者:${COLOR_RESET} 唐初稚 (Discord)"
+    echo -e "  ${COLOR_MAGENTA}发布:${COLOR_RESET} 游鹿小岛"
     echo -e "${COLOR_GRAY}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${COLOR_RESET}"
 }
